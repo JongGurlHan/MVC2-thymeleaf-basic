d60fdb02581f17dbfdcf6d250c432b572536fdb7
JG HAN <gojkhan13@gmail.com>
2021-08-10 01:50:02 +0900

    0809 타임리프 -리터럴

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -83,7 +83,22 @@ public class BasicController {
         return "basic/date";
     }
 
+    //URL링크
+    @GetMapping("link")
+    public String link(Model model){
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "basic/link";
+    }
 
+
+
+    //리터럴
+    @GetMapping("/literal")
+    public String literal(Model model){
+        model.addAttribute("data", "world!");
+        return "basic/literal";
+    }
 
 
 
