42ff950c00ac933c473bd1aff2d4db7596a39073
JG HAN <gojkhan13@gmail.com>
2021-08-09 23:31:43 +0900

    0809 타임리프 -날짜

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,11 +1,14 @@
 package hello.thymeleaf.basic;
 
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -27,6 +30,7 @@ public class BasicController {
         return "basic/text-unescaped";
     }
 
+//    변수 - SpringEL
     @GetMapping("/variable")
     public String variable(Model model){
         User userA = new User("userA", 10);
@@ -58,6 +62,28 @@ public class BasicController {
             this.age = age;
         }
     }
+    //기본 객체들
+    @GetMapping("/basic-objects")
+    public String basicObjects(HttpSession session){
+        session.setAttribute("sessionData", "Hello Session"); //session: 웹브라우저 종료하기 전까진 데이터 유지
+        return "basic/basic-objects";
+    }
+
+    @Component("helloBean") //component scan해서  HelloBean이 스프링빈으로 등록됨.
+    static class HelloBean{
+        public String hello(String data){
+            return "Hello " + data;
+        }
+    }
+
+    //유틸리티 객체와 날짜
+    @GetMapping("/date")
+    public String date(Model model){
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
+    }
+
+
 
 
 
