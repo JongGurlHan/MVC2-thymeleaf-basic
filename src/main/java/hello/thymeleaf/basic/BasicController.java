6669c7e4dcb6a4a659354fa72772c6d9bf45448a
JG HAN <gojkhan13@gmail.com>
2021-08-11 10:33:34 +0900

    0811 타임리프 -템플릿 조각

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -132,6 +132,16 @@ public class BasicController {
         return "basic/block";
     }
 
+    //자바스크립트 인라인
+    @GetMapping("/javascript")
+    public String javascript(Model model){
+        model.addAttribute("user", new User("UserA", 10));
+        addUsers(model);
+
+        return "basic/javascript";
+    }
+
+
     private void addUsers(Model model){
         List<User>list = new ArrayList<>();
         list.add(new User("UserA", 10));
