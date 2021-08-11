8744847929f47dfc3f663fb4fd6c4d1dc0db2c89
JG HAN <gojkhan13@gmail.com>
2021-08-11 23:41:27 +0900

    0811 타임리프 -템플릿 레이아웃2

--- src/main/java/hello/thymeleaf/basic/TemplateController.java
+++ src/main/java/hello/thymeleaf/basic/TemplateController.java
@@ -8,8 +8,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/template")
 public class TemplateController {
 
+    //템플릿 조각
     @GetMapping("/fragment")
     public String template() {
         return "template/fragment/fragmentMain";
+    }
+
+    //템플릿 레이아웃1
+    @GetMapping("/layout")
+    public String layout(){
+        return "template/layout/layoutMain";
+    }
+
+    //템플릿 레이아웃2
+    @GetMapping("/layoutExtend")
+    public String layoutExtend(){
+        return "template/layoutExtend/layoutExtendMain";
     }
 }
